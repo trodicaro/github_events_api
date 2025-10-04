545e0f81d249b8dfa085e6cb5c98a391840f7a61
Ro Tr <trodicaro@gmail.com>
2025-10-03 23:15:10 -0500

    Add test for get_owned_repos

--- test_script.py
+++ test_script.py
@@ -1,5 +1,14 @@
 import pytest
-from script import (extract_next_url, get_top_activities)
+from script import (extract_next_url, get_top_activities, get_owned_repos)
+
+@pytest.fixture
+def sample_events():
+    return [
+        {'type': 'PushEvent', 'repo': {'name': 'user/repo1'}},
+        {'type': 'PushEvent', 'repo': {'name': 'user/repo2'}},
+        {'type': 'IssuesEvent', 'repo': {'name': 'other/repo3'}},
+    ]
+
 
 @pytest.mark.parametrize("link_header,expected", [
     ('<https://api.github.com/page2>; rel="next"', 'https://api.github.com/page2'),
@@ -17,3 +26,15 @@ def test_extract_next_url(link_header, expected):
 ])
 def test_get_top_activities(events, expected):
     assert get_top_activities(events, 3) == expected
+
+
+@pytest.mark.parametrize("username,top_activities,expected", [
+    ('user', ['PushEvent'], {'user/repo1', 'user/repo2'}),
+    ('user', ['IssuesEvent'], set()),
+    ('user', ['ForkEvent'], set()),
+    ('other', ['IssuesEvent'], {'other/repo3'}),
+    ('user', ['PushEvent', 'IssuesEvent'], {'user/repo1', 'user/repo2'}),
+])
+def test_get_owned_repos(sample_events, username, top_activities, expected):
+    result = get_owned_repos(sample_events, username, top_activities)
+    assert result == expected
