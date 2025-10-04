beda9536f73a269ec98c418b6b8d9139b52936bd
Ro Tr <trodicaro@gmail.com>
2025-10-03 23:00:00 -0500

    Add test for get_top_activities

--- test_script.py
+++ test_script.py
@@ -1,5 +1,5 @@
 import pytest
-from script import extract_next_url
+from script import (extract_next_url, get_top_activities)
 
 @pytest.mark.parametrize("link_header,expected", [
     ('<https://api.github.com/page2>; rel="next"', 'https://api.github.com/page2'),
@@ -9,3 +9,11 @@ from script import extract_next_url
 ])
 def test_extract_next_url(link_header, expected):
     assert extract_next_url(link_header) == expected
+
+@pytest.mark.parametrize("events,expected", [
+    ([{'type': 'Push'}, {'type': 'Push'}, {'type': 'Issue'}], ['Push', 'Issue']),
+    ([{'type': 'Push'}], ['Push']),
+    ([{'type': 'A'}, {'type': 'B'}, {'type': 'C'}, {'type': 'D'}], ['A', 'B', 'C']),
+])
+def test_get_top_activities(events, expected):
+    assert get_top_activities(events, 3) == expected
