cbe9c776e92e13a7cf2a8a21319246e3be89177b
Ro Tr <trodicaro@gmail.com>
2025-10-03 22:54:56 -0500

    Parametrize for various cases for link header

--- test_script.py
+++ test_script.py
@@ -1,6 +1,11 @@
 import pytest
 from script import extract_next_url
 
-def test_extract_next_url_found():
-    link = '<https://api.github.com/page2>; rel="next"'
-    assert extract_next_url(link) == 'https://api.github.com/page2'
+@pytest.mark.parametrize("link_header,expected", [
+    ('<https://api.github.com/page2>; rel="next"', 'https://api.github.com/page2'),
+    ('<https://api.github.com/page1>; rel="prev"', None),
+    ('<https://api.github.com/page2>; rel="Next"', 'https://api.github.com/page2'),
+    ('', None),
+])
+def test_extract_next_url(link_header, expected):
+    assert extract_next_url(link_header) == expected
